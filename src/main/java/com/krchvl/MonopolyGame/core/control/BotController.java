6a262d0a415e18d147b28cf820ea1c4ae3f72bf6
Vladislav <vladoqqik@icloud.com>
2025-11-23 21:40:44 +0300

    feat(core): Now bots can upgrade tiles

--- src/main/java/com/krchvl/MonopolyGame/core/control/BotController.java
+++ src/main/java/com/krchvl/MonopolyGame/core/control/BotController.java
@@ -1,16 +1,19 @@
 package com.krchvl.MonopolyGame.core.control;
 
+import com.krchvl.MonopolyGame.core.Board;
 import com.krchvl.MonopolyGame.core.Player;
 import com.krchvl.MonopolyGame.core.engine.GameEngine;
 import com.krchvl.MonopolyGame.core.tiles.CompanyTile;
+import com.krchvl.MonopolyGame.core.tiles.Tile;
 
 import static com.krchvl.MonopolyGame.core.engine.GameEngine.Phase;
 
 public class BotController implements PlayerController {
     private final int reserve;
 
-    private static final int BASE_STEP_DURATION = 200; // из GameController
+    private static final int BASE_STEP_DURATION = 200;
     private static final int MAX_DICE_ROLL = 12;
+    private static final int THINKING_TIME = 800;
 
     public BotController(int reserve) { this.reserve = reserve; }
 
@@ -23,22 +26,52 @@ public class BotController implements PlayerController {
         switch (ph) {
             case AWAITING_ROLL:
             case AWAITING_ROLL_AGAIN:
-                delayThen(800, engine::roll);
+                delayThen(THINKING_TIME, engine::roll);
                 break;
 
             case AWAITING_BUY_DECISION:
                 int maxAnimationTime = BASE_STEP_DURATION * MAX_DICE_ROLL;
-                int thinkingTime = 800;
-                delayThen(maxAnimationTime + thinkingTime, () -> handleBuyDecision(engine));
+                delayThen(maxAnimationTime + THINKING_TIME, () -> handleBuyDecision(engine));
                 break;
 
             case AWAITING_END_TURN:
-                delayThen(600, engine::endTurn);
+                tryUpgradeOrEndTurn(engine);
                 break;
 
             default:
                 break;
         }
+    }
+
+    private void tryUpgradeOrEndTurn(GameEngine engine) {
+        delayThen(600, () -> {
+            CompanyTile tileToUpgrade = findTileToUpgrade(engine);
+
+            if (tileToUpgrade != null) {
+                engine.upgradeTile(tileToUpgrade);
+                tryUpgradeOrEndTurn(engine);
+            } else {
+                engine.endTurn();
+            }
+        });
+    }
+
+    private CompanyTile findTileToUpgrade(GameEngine engine) {
+        Player p = engine.current();
+        Board board = engine.getBoard();
+
+        for (int i = 0; i < board.size(); i++) {
+            Tile t = board.getTile(i);
+            if (t instanceof CompanyTile ct) {
+                if (engine.canUpgradeTile(p, ct)) {
+                    int cost = engine.calcTileUpgradeCost(ct);
+                    if (p.getBalance() - cost >= reserve) {
+                        return ct;
+                    }
+                }
+            }
+        }
+        return null;
     }
 
     private void handleBuyDecision(GameEngine engine) {
